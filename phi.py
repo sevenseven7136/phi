3eee798ad9e30c715904bae094c31a548ad54615
sevenseven7136 <tourou2.kamakiri@gmail.com>
2025-04-04 22:12:50 +0900

    added Phi_S and phi_LL

--- phi.py
+++ phi.py
@@ -1,9 +1,9 @@
-def Tau(f):
+def Tau(f): #タウ変換
     def g(x):
         return f(f(x))
     return g
 
-def Phi(f,n):
+def Phi(f,n): #ファイ変換
     if n==0:
         return f
     else:
@@ -12,13 +12,29 @@ def Phi(f,n):
 def f_0(x):
     return x**x
 
-def phi_l(x):
+def phi_l(x): #小ファイ関数
     return Phi(Phi(Tau,x),x)(f_0)(x)
 
-def phi_L(x):
+def phi_L(x): #大ファイ関数
     def Phi_x(f):
         return Phi(f,x)
     t = Tau
     for i in range(x):
         t = Phi_x(t)
     return t(f_0)(x)
+
+def phi_L_v2(x): #大ファイ関数v2
+    def Phi_x(f):
+        return Phi(f,x)
+    return Phi_x(Tau)(Phi_x)(Tau)(f_0)(x)
+
+def Phi_S(f,g,n): #強ファイ変換
+    if n==0:
+        return g(f)
+    else:
+        return Phi_S(f,g,n-1)(g)(f)
+
+def phi_LL(x): #強大ファイ関数
+    def Phi_x(f):
+        return Phi(f,x)
+    return Phi_S(Tau,Phi_x,x)(f_0)(x)
